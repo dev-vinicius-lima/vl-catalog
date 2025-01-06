ab6fb4799a4647d486a2afe68b861faf0ea33cff
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-01-06 16:16:02 -0400

    feat: :sparkles: create project java with spring boot

---
+++ backend/src/main/java/com/viniciuslima/dscatalog/DscatalogApplication.java
@@ -0,0 +1,14 @@
+package com.viniciuslima.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DscatalogApplication.class, args);
+        System.out.println("Projeto iniciado com sucesso!");
+    }
+
+}
