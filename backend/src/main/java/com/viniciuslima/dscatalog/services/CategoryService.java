7a5e43f118185c7acc34eab5cff34a4fcfe0d09e
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-01-07 10:15:14 -0400

    feat: :sparkles: find By Category Id

--- backend/src/main/java/com/viniciuslima/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/viniciuslima/dscatalog/services/CategoryService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -20,5 +21,12 @@ public class CategoryService {
     public List<CategoryDTO> findAll() {
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        Category entity = obj.get();
+        return new CategoryDTO(entity);
     }
 }
