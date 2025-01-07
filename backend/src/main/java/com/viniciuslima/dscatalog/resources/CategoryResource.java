177b0bd3dcc9defb63c3eede4a9cb107edda58cc
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-01-07 09:57:04 -0400

    feat: :sparkles: create CategoryDTO

--- backend/src/main/java/com/viniciuslima/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/viniciuslima/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,6 @@
 package com.viniciuslima.dscatalog.resources;
 
-import com.viniciuslima.dscatalog.entities.Category;
+import com.viniciuslima.dscatalog.dto.CategoryDTO;
 import com.viniciuslima.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,8 +18,8 @@ public class CategoryResource {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> categoryList = service.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categoryList = service.findAll();
         return ResponseEntity.ok(categoryList);
     }
 }
