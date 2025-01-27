65c42bb8e684ae7ea3f7f6c1d1d2d0bf224ebbd9
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2025-01-27 14:21:05 -0400

    feat: :sparkles: add Signup for user cadaster with role operator

--- backend/src/main/java/com/viniciuslima/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/viniciuslima/dscatalog/dto/UserInsertDTO.java
@@ -1,9 +1,15 @@
 package com.viniciuslima.dscatalog.dto;
 
 import com.viniciuslima.dscatalog.services.validation.UserInsertValid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 @UserInsertValid
 public class UserInsertDTO extends UserDTO {
+
+    @NotBlank(message = "A senha é obrigatória")
+    @Size(min = 8, message = "A senha deve ter pelo menos 8 caracteres")
     private String password;
 
     public UserInsertDTO() {
